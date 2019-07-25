35b16b5c7144cb9cf60e5e81e711fa0776a1ff7e
Shradha Menon <39404261+shradhamenon@users.noreply.github.com>
2019-07-25 13:10:33 +0530

    Update spt_keys.py

--- spt_keys.py
+++ spt_keys.py
@@ -1,5 +1,5 @@
 KEYS = {
-    "SPOTIFY_CLIENT_ID": "8e564904a1844f099ccfad1e3accc38d",  
-    "SPOTIFY_CLIENT_SECRET": "53c81d8ba4b148afaaa511e86fbb8cb5",
+    "SPOTIFY_CLIENT_ID": "",  
+    "SPOTIFY_CLIENT_SECRET": "",
     "SPOTIFY_REDIRECT_URI": "http://127.0.0.1:5000/callback/q",  
 }
